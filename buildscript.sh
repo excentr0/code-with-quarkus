522da39549d1513ad7afc683a2b9b4d24272f1a1
Михаил Мишин <mmishin@xdsoft.ru>
2024-01-11 14:46:15 +0300

    Added a buildscript.sh

---
+++ buildscript.sh
@@ -0,0 +1,1 @@
+./mvnw, clean, package, -f, pom.xml, -B, -V, -e, -Dfindbugs.skip, -Dcheckstyle.skip, -Dpmd.skip=true, -Dspotbugs.skip, -Denforcer.skip, -Dmaven.javadoc.skip, -DskipTests, -Dmaven.test.skip.exec, -Dlicense.skip=true, -Drat.skip=true, -Dspotless.check.skip=true
